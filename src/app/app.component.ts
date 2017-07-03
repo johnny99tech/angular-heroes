f00caed61081f028dac3996bfbddb2505b62dd34
johnny99tech <jonathan99moura@hotmail.com>
2017-07-03 10:57:14 -0300

    passo 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+/*
 export class Hero {
   id: number;
   name: string;
-}
+}*/
+
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -21,12 +24,25 @@ const HEROES: Hero[] = [
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <hero-detail [hero]="selectedHero"></hero-detail>
+`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'TII App';
   heroes = HEROES;
   selectedHero : Hero = undefined;
+
   onSelect(hero : Hero) : void {
     this.selectedHero = hero;
   };
